efd958e209134243749e87c2076dc3fd7819ab9c
LeftRadio <vladislav@inorbit.com>
2018-02-01 20:28:20 +0200

    units/py/mqtt_base.py: add alt_settings field as concentration client to server settings

--- units/py/mqtt.py
+++ units/py/mqtt.py
@@ -1,75 +1,134 @@
-#
+
+from uhashlib import sha256
+from ubinascii import hexlify
 from mqtt_base import MQTTClientBase
-from uplatform import json
+
+from defaults import MODULE_SERIAL_HASH
+from uplatform import utime, json
 
 
 class MQTTClient(MQTTClientBase):
-
-    propertylist = b'alt_settings,alt_index,keepalive,ssl,ssl_params'
+    propertylist = b'alt_settings,alt_index'
 
     def __init__(self, **kwargs):
         super(MQTTClient, self).__init__(**kwargs)
-        self._name = kwargs['name']
-        self.state = b'reset'
-        self.state_cb = lambda x: x
+        # load default settings
+        if not len(self.alt_settings):
+            self.alt_settings = MQTT_SETTINGS
+        # server trust flag, set True only if module recived
+        # self serial, then calc sha256 and compare with stored hash.
+        self._server_trust = False
+        # client state and state callback
+        self.state = b'server_lost'
+        self.state_callback = None
+
+    def _except_setstate(self, ex):
+        ex = str(ex)
+        if '10035' in ex or 'TIMEDOUT' in ex:
+            return
+        if 'ABORT' in ex:
+            self.set_property('state', b'server_lost')
+        else:
+            self.set_property('state', b'reset')
+        self.disconnect()
+
+    def _auth_server_request(self, try_cnt=3):
+        while try_cnt:
+            super().publish('services/client-auth', self.name, False, 0)
+            utime.sleep(1)
+            self.check_message(trustonly=False)
+            if self._server_trust:
+                return
+            try_cnt -= 1
+        raise OSError(-1)
+
+    def _auth_server_respond(self, topic, msg):
+        if b'client-auth' in topic:
+            msg_hash = sha256(msg)
+            if (hexlify(msg_hash.digest()) == MODULE_SERIAL_HASH):
+                self._server_trust = True
+                return
+        raise OSError(-1)
 
     def _recive_msg(self, topic, msg):
-        if self.recived_cb is not None:
-            topic = [ s for s in str(topic, 'utf-8').split('/') if s != '' ]
+        # check server auth state
+        if not self._server_trust:
+            self._auth_server_respond(topic, msg)
+        # normal recive
+        elif self.recived_cb:
+            stopic = [ s for s in str(topic, 'utf-8').split('/') if s != '' ]
             data = json.loads(str(msg, 'utf-8')) if msg else None
-            self.recived_cb( topic[-2], topic[-1], data )
+            self.recived_cb( stopic[-2], stopic[-1], data )
 
-    def _ex_upd_state(self, ex):
-        if 'ECONNABORTED' in str(ex):
-            self.set_property('state', b'server_lost')
-        elif '10035' in str(ex):
-            return
-        else:
-            self.disconnect()
-            self.set_property('state', b'reset')
+    def group(self):
+        return None
 
-    def set_property(self, attr, value):
-        if attr == 'state':
-            if self.state != value:
-                setattr(self, attr, value)
-                self.state_cb(value)
+    def properties(self, **kwargs):
+        if not len(kwargs):
+            return {
+                'classname': self.__class__.__name__,
+                'name': self.name,
+                'alt_index': self.alt_index,
+                'alt_settings': self.alt_settings
+            }
+        for k, v in kwargs.values():
+            self.set_property(k, v)
+
+    def set_property(self, key, value):
+        if key[0] == '_':
             return
-        elif attr == '_alt_index':
-            if value >= len(self._alt_settings):
+        elif key == 'state':
+            if self.state_callback:
+                self.state_callback(value)
+        elif key == 'alt_index':
+            if value >= len(self.alt_settings):
                 value = 0
-        setattr(self, attr, value)
+            self.state = b'reset'
+        setattr(self, key, value)
 
     def init(self):
         pass
 
-    def update(self, **kwargv):
+    def update(self, seconds, interval):
         if self.state == b'reset':
             self.connect()
-        self.check_message()
+        elif self._server_trust:
+            self.check_message()
 
-    def connect(self):
+    def connect(self, timeout=1):
+        self._server_trust = False
         try:
-            super().connect(0.5, True)
-            self.subscribe(self._name+"/#")
+            super().connect(timeout, True)
+            self.subscribe(self.name+"/rx/#")
+            self._auth_server_request(timeout)
             self.set_property('state', b'connected')
+        except Exception as e:
+            self._except_setstate(b'ABORT CONNECT')
+
+    def disconnect(self):
+        self._server_trust = False
+        self.set_property('state', b'reset')
+        try:
+            super().disconnect()
         except Exception:
-            self._ex_upd_state(b'ECONNABORTED')
+            pass
 
     def publish(self, topic, msg, retain=False, qos=0):
-        if self.state != b'connected':
+        if not self._server_trust:
             return
         try:
-            topic = '%s/%s/' % (self._name, topic)
+            topic = '%s/tx/%s/' % (self.name, topic)
             msg = bytes( json.dumps(msg), 'utf-8')
             super().publish(topic, msg, retain, qos)
         except Exception as e:
-            self._ex_upd_state(e)
+            self._except_setstate(e)
 
-    def check_message(self):
-        if self.state != b'connected':
+    def check_message(self, trustonly=True, timeout=0.05):
+        if trustonly and not self._server_trust:
             return
         try:
             while 1:
-                super().check_msg()
+                super().check_msg(timeout)
         except Exception as e:
-            self._ex_upd_state(e)
+            self._except_setstate(e)
+
--- units/py/mqtt_base.py
+++ units/py/mqtt_base.py
@@ -1,12 +1,15 @@
 
 import ustruct as struct
 import usocket as socket
+import ussl
 from bmdns import get_server_ip as get_localserver
+from defaults import MQTT_NAME_ID, MQTT_SETTINGS
 
 
 class MQTTClientBase(object):
-    '''docstring for MQTTClientBase'''
+
     def __init__(self, **kwargs):
+        self._sock = None
         self._sock = None
         self._pid = 0
         self._lw_topic = None
@@ -14,12 +17,9 @@ class MQTTClientBase(object):
         self._lw_qos = 0
         self._lw_retain = False
 
-        self._alt_settings = kwargs.get('alt_settings', [])
-        self._alt_index = kwargs.get('alt_index', 0)
-
-        self._keepalive = kwargs.get('keepalive', 60)
-        self._ssl = kwargs.get('ssl', False)
-        self._ssl_params = kwargs.get('ssl_params', {})
+        self.name = kwargs.get('name', MQTT_NAME_ID)
+        self.alt_settings = kwargs.get('alt_settings', MQTT_SETTINGS)
+        self.alt_index = kwargs.get('alt_index', 0)
 
         self.connected_cb = None
         self.recived_cb = None
@@ -31,24 +31,18 @@ class MQTTClientBase(object):
         out.extend(bytes(s, 'utf-8'))
         self._sock.write(out)
 
-    def _recive(self, lenb):
-        res = self._sock.read(lenb)
-        if res is None:
-            raise OSError(10035)
-        return res
-
     def _recv_len(self):
         n = 0
         sh = 0
         while 1:
-            b = self._recive(1)[0]
+            b = self._sock.read(1)[0]
             n |= (b & 0x7f) << sh
             if not b & 0x80:
                 return n
             sh += 7
 
     def _recive_msg(self, topic, msg):
-        if self.recived_cb is not None:
+        if self.recived_cb:
             self.recived_cb(str(topic, 'utf-8'), str(msg, 'utf-8'))
 
     def set_last_will(self, topic, msg, retain=False, qos=0):
@@ -60,67 +54,58 @@ class MQTTClientBase(object):
         self._lw_retain = retain
 
     def connect(self, timeout=1, clean_session=True):
-        assert len(self._alt_settings)
-        serv, port, user, pswd = self._alt_settings[self._alt_index].split(',')
+        assert len(self.alt_settings)
+        serv, port, user, pswd, ssl, ca_certs, keepalive = self.alt_settings[self.alt_index].split(',')
         if serv.endswith('.local'):
             serv = get_localserver(serv, timeout=timeout)
             assert serv is not None
         addr_info = socket.getaddrinfo(serv, int(port))[0][-1]
-        self._sock = socket.socket()
-        self._sock.settimeout(timeout)
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock.setblocking(True)
         self._sock.connect(addr_info)
-        if self._ssl:
-            import ussl
-            self._sock = ussl.wrap_socket(self._sock, **self._ssl_params)
+        if ssl:
+            self._sock = ussl.wrap_socket(self._sock)
         msg = bytearray(b'\x10\x00\x00\x04MQTT\x04\x02\x00\x00')
-        msg[1] = 10 + 2 + len(self._name)
+        msg[1] = 10 + 2 + len(self.name)
         msg[9] = clean_session << 1
         if user:
             msg[1] += 2 + len(user) + 2 + len(pswd)
             msg[9] |= (0x03) << 6
-        if self._keepalive:
-            assert self._keepalive < 65536
-            msg[10] |= self._keepalive >> 8
-            msg[11] |= self._keepalive & 0x00FF
+        if keepalive:
+            keepalive = int(keepalive)
+            assert keepalive < 65536
+            msg[10] |= keepalive >> 8
+            msg[11] |= keepalive & 0x00FF
         if self._lw_topic:
             msg[1] += 2 + len(self._lw_topic) + 2 + len(self._lw_msg)
             msg[9] |= 0x4 | (self._lw_qos & 0x1) << 3 | (self._lw_qos & 0x2) << 3
             msg[9] |= self._lw_retain << 5
         self._sock.write(msg)
-        self._send_str(self._name)
+        self._send_str(self.name)
         if self._lw_topic:
             self._send_str(self._lw_topic)
             self._send_str(self._lw_msg)
         if user:
             self._send_str(user)
             self._send_str(pswd)
-        resp = self._recive(4)
+        resp = self._sock.read(4)
         if resp is None or not len(resp):
             raise OSError(-1)
         assert resp[0] == 0x20 and resp[1] == 0x02
         if resp[3] != 0:
             raise OSError(resp[3])
-        if self.connected_cb is not None:
+        if self.connected_cb:
             self.connected_cb(resp[2] & 0x01, addr_info)
-        return (resp[2] & 0x01, addr_info)
 
     def disconnect(self):
-        try:
-            self._sock.write(b'')
-        except Exception:
-            pass
-        try:
-            self._sock.close()
-        except Exception:
-            pass
+        self._sock.write(b'\xe0\x00\x00\x00')
+        self._sock.close()
         if self.disconnect_cb:
             self.disconnect_cb()
 
     def ping(self):
-        try:
-            self._sock.write(b' ')
-        except OSError:
-            pass
+        self._sock.write(b'\xc0\x00\x00\x00')
+        self.check_msg()
 
     def publish(self, topic, msg, retain=0, qos=0):
         if qos == 2:
@@ -146,9 +131,9 @@ class MQTTClientBase(object):
             while 1:
                 op = self.wait_msg()
                 if op == 0x40:
-                    sz = self._recive(1)
+                    sz = self._sock.read(1)
                     assert sz == b''
-                    rcv_pid = self._recive(2)
+                    rcv_pid = self._sock.read(2)
                     rcv_pid = rcv_pid[0] << 8 | rcv_pid[1]
                     if pid == rcv_pid:
                         break
@@ -166,31 +151,31 @@ class MQTTClientBase(object):
                 resp = self._sock.read(4)
                 assert resp[1] == pkt[2] and resp[2] == pkt[3]
                 if resp[3] == 0x80:
-                    raise OSError(resp[3])
+                    raise OSError(-1)
                 return
 
     def wait_msg(self):
-        res = self._recive(1)
+        res = self._sock.read(1)
+        if not res:
+            raise OSError(10035)
         self._sock.setblocking(True)
-        if res == b'':
-            raise OSError(-1)
         if res == b'\xd0':  # PINGRESP
-            sz = self._recive(1)[0]
+            sz = self._sock.read(1)[0]
             assert sz == 0
             return None
         op = res[0]
         if op & 0xf0 != 0x30:
             return op
         sz = self._recv_len()
-        topic_len = self._recive(2)
+        topic_len = self._sock.read(2)
         topic_len = (topic_len[0] << 8) | topic_len[1]
-        topic = self._recive(topic_len)
+        topic = self._sock.read(topic_len)
         sz -= topic_len + 2
         if op & 6:
-            pid = self._recive(2)
+            pid = self._sock.read(2)
             pid = pid[0] << 8 | pid[1]
             sz -= 2
-        msg = self._recive(sz)
+        msg = self._sock.read(sz)
         if op & 6 == 2:
             pkt = bytearray([0x40, 0x02, 0x00, 0x00])
             struct.pack_into('!H', pkt, 2, pid)
@@ -200,6 +185,7 @@ class MQTTClientBase(object):
         self._recive_msg(topic, msg)
         return op
 
-    def check_msg(self):
-        self._sock.setblocking(False)
+    def check_msg(self, timeout=0.1):
+        # self._sock.setblocking(False)
+        self._sock.settimeout(timeout)
         return self.wait_msg()
